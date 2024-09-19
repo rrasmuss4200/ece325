fc62dba125a1642455d4f2253b7c0b6811ec48d4
rrasmuss07 <rrasmuss@ualberta.ca>
2024-09-19 16:52:21 -0400

    lab1

---
+++ labs/ece325_lab_assignment1/lab_assignment1/src/ece325/labs/lab1/BandNameGenerator.java
@@ -0,0 +1,143 @@
+
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class BandNameGenerator {
+	String[] adjectives;
+	String[] nouns;
+
+	boolean adjectivesLoaded = false;
+	boolean nounsLoaded = false;
+
+	String adjectivesFile;
+	String nounsFile;
+
+	public BandNameGenerator(String adjectivesFile, String nounsFile) {
+		this.adjectivesFile = adjectivesFile;
+		this.nounsFile = nounsFile;
+	}
+
+
+	/**
+	 * Load the adjectives file and initialize that part of the generator.
+	 */
+	public void loadAdjectives() {
+		adjectives = loadTxt(adjectivesFile);
+
+		if (adjectives.length > 0) {
+			adjectivesLoaded = true;
+		} else {
+			System.err.println("Error loading adjectives from: " + adjectivesFile);
+		}
+	}
+
+	/**
+	 * Load the nouns file and initialize that part of the generator.
+	 */
+	public void loadNouns() {
+		nouns = loadTxt(nounsFile);
+
+		if (nouns.length > 0) {
+			nounsLoaded = true;
+		} else {
+			System.err.println("Error loading adjectives from: " + nounsFile);
+		}
+	}
+
+	/**
+	 * Return a string that capitalizes the first letter of s and lowercases the rest. So for example,
+	 * capitalizeFirst('cat') returns 'Cat', and capitalizeFirst('CHUBBY CAt') returns 'Chubby cat'. Read the assignment carefully and think about how
+	 * to implement this part of the code.
+	 *
+	 * @param s
+	 * @return
+	 */
+	public String capitalizeFirst(String s) {
+		String y = s.substring(0, 1).toUpperCase() + s.substring(1);
+		return y;
+	}
+
+
+	/**
+	 * Generate a name for your band consisting of two adjectives and one noun.
+	 * Make sure to return "UNINITIALIZED" if the band name generator is not initialized correctly yet.
+	 * @return the generated name or "UNINITIALIZED"
+	 */
+	public String generateName() {
+		int randIndexAdj1 = (int) (Math.random() * 1348);
+		int randIndexAdj2 = (int) (Math.random() * 1348);
+		int randIndexNoun = (int) (Math.random() * 1525);
+		return "";
+	}
+
+
+	/**
+	 * This method loads a text file into a String array. It assumes the number of
+	 * lines in the file is on the first line of the file itself.
+	 *
+	 * Note: you are not allowed to make changes to this method. You can use this method for
+	 * loading text files in the other lab and course assignments as well.
+	 *
+	 * @param file
+	 * @return
+	 */
+	private String[] loadTxt(String file) {
+		String[] data = new String[0];
+		BufferedReader in = null;
+
+		try {
+			in = new BufferedReader(new FileReader(file));
+			String line;
+			int i = 0;
+			int totalLines = Integer.parseInt(in.readLine());
+			data = new String[totalLines];
+			while((line = in.readLine()) != null)
+			{
+				data[i] = line;
+				i++;
+
+			}
+		} catch (Exception e) {
+			System.err.println("Problem while reading file: " + file);
+			e.printStackTrace();
+		}
+		finally {
+			if(in != null) {
+				try {
+					in.close();
+				} catch (IOException e) {
+					System.err.println("Problem closing file: " + file);
+					e.printStackTrace();
+				}
+			}
+		}
+		return data;
+	}
+
+	/**
+	 * Returns the list of adjectives.
+	 * @return
+	 */
+	public String[] getAdjectives() {
+		return adjectives;
+	}
+
+	/**
+	 * Returns the list of nouns.
+	 * @return
+	 */
+	public String[] getNouns() {
+		return nouns;
+	}
+
+	public static void main(String[] args) {
+		// create a BandNameGenerator and initialize it
+
+		// generate and print 20 names for your band
+
+
+	}
+
+}
